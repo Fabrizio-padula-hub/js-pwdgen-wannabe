a4c3fa23cbde459cc59e9837887c46f6526b527a
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-03-04 15:39:19 +0100

    feat: add js to generate passwords

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 // GENERATORE DI PASSWORD
 
 // Chiedi all’utente il suo nome
+const userName = prompt('Scrivi il tuo nome');
 
 // poi chiedi il suo cognome
+const userLastName = prompt('Scrivi il tuo cognome');
 
 // poi chiedi il suo colore preferito
+const color = prompt('Scrivi il tuo colore preferito');
+
 
 // Infine scrivi sulla pagina nomecognomecolorepreferito23
+let Result = `la tua password è: ${userName}${userLastName}${color}${'23'}`;
+document.getElementById('result').innerHTML = Result;
+console.log(Result);
