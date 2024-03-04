d7d7fd81694a47347cc95e1644084be076f12a63
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-03-04 15:12:03 +0100

    adventure js

---
+++ script.js
@@ -0,0 +1,9 @@
+// GENERATORE DI PASSWORD
+
+// Chiedi all’utente il suo nome
+
+// poi chiedi il suo cognome
+
+// poi chiedi il suo colore preferito
+
+// Infine scrivi sulla pagina nomecognomecolorepreferito23
